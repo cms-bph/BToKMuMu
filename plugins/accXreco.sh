0f10586aa279458f6df0ef29f7ad35dd5d0b369f
Geng Chen <normanaachen@gmail.com>
2015-01-25 09:54:42 +0100

    Collected command

--- plugins/accXreco.sh
+++ plugins/accXreco.sh
@@ -1,15 +1,15 @@
 
-./fit accXrecoEff mc ../RootFiles/AllCut/MC_Signal_AllCuts_v3.root 999 >& ./fitParameters/LogFiles/acc_999.log
+./fit accXrecoEff mc ../RootFiles/AllCut/MC_Signal_AllCuts_v3.root 999 -b 
 
-./fit accXrecoEff mc ../RootFiles/AllCut/MC_Signal_AllCuts_v3.root 0 >& ./fitParameters/LogFiles/eff_0.log
-./fit accXrecoEff mc ../RootFiles/AllCut/MC_Signal_AllCuts_v3.root 1 >& ./fitParameters/LogFiles/eff_1.log
-./fit accXrecoEff mc ../RootFiles/AllCut/MC_Signal_AllCuts_v3.root 2 >& ./fitParameters/LogFiles/eff_2.log
-#./fit accXrecoEff mc ../RootFiles/AllCut/MC_Signal_AllCuts_v3.root 3 >& ./fitParameters/LogFiles/eff_3.log
-./fit accXrecoEff mc ../RootFiles/AllCut/MC_Signal_AllCuts_v3.root 4 >& ./fitParameters/LogFiles/eff_4.log
-#./fit accXrecoEff mc ../RootFiles/AllCut/MC_Signal_AllCuts_v3.root 5 >& ./fitParameters/LogFiles/eff_5.log
-./fit accXrecoEff mc ../RootFiles/AllCut/MC_Signal_AllCuts_v3.root 6 >& ./fitParameters/LogFiles/eff_6.log
-./fit accXrecoEff mc ../RootFiles/AllCut/MC_Signal_AllCuts_v3.root 7 >& ./fitParameters/LogFiles/eff_7.log
-./fit accXrecoEff mc ../RootFiles/AllCut/MC_Signal_AllCuts_v3.root 8 >& ./fitParameters/LogFiles/eff_8.log
-./fit accXrecoEff mc ../RootFiles/AllCut/MC_Signal_AllCuts_v3.root 9 >& ./fitParameters/LogFiles/eff_9.log
-./fit accXrecoEff mc ../RootFiles/AllCut/MC_Signal_AllCuts_v3.root 10 >& ./fitParameters/LogFiles/eff_10.log
+./fit accXrecoEff mc ../RootFiles/AllCut/MC_Signal_AllCuts_v3.root 0 -b 
+./fit accXrecoEff mc ../RootFiles/AllCut/MC_Signal_AllCuts_v3.root 1 -b 
+./fit accXrecoEff mc ../RootFiles/AllCut/MC_Signal_AllCuts_v3.root 2 -b 
+#./fit accXrecoEff mc ../RootFiles/AllCut/MC_Signal_AllCuts_v3.root 3 -b 
+./fit accXrecoEff mc ../RootFiles/AllCut/MC_Signal_AllCuts_v3.root 4 -b 
+#./fit accXrecoEff mc ../RootFiles/AllCut/MC_Signal_AllCuts_v3.root 5 -b 
+./fit accXrecoEff mc ../RootFiles/AllCut/MC_Signal_AllCuts_v3.root 6 -b 
+./fit accXrecoEff mc ../RootFiles/AllCut/MC_Signal_AllCuts_v3.root 7 -b 
+./fit accXrecoEff mc ../RootFiles/AllCut/MC_Signal_AllCuts_v3.root 8 -b 
+./fit accXrecoEff mc ../RootFiles/AllCut/MC_Signal_AllCuts_v3.root 9 -b 
+./fit accXrecoEff mc ../RootFiles/AllCut/MC_Signal_AllCuts_v3.root 10 -b 
 
--- plugins/gen.sh
+++ plugins/gen.sh
@@ -1,17 +1,17 @@
 
-./fit angular_gen mc.gen ../RootFiles/MC_GENOnly/BToKMuMu_GENOnly_8TeV_genonly_v3-1.root 0 >& ./fitParameters/LogFiles/gen_0.log
-./fit angular_gen mc.gen ../RootFiles/MC_GENOnly/BToKMuMu_GENOnly_8TeV_genonly_v3-1.root 1 >& ./fitParameters/LogFiles/gen_1.log
-./fit angular_gen mc.gen ../RootFiles/MC_GENOnly/BToKMuMu_GENOnly_8TeV_genonly_v3-1.root 2 >& ./fitParameters/LogFiles/gen_2.log
-#./fit angular_gen mc.gen ../RootFiles/MC_GENOnly/BToKMuMu_GENOnly_8TeV_genonly_v3-1.root 3 >& ./fitParameters/LogFile/sgen_3.log
-./fit angular_gen mc.gen ../RootFiles/MC_GENOnly/BToKMuMu_GENOnly_8TeV_genonly_v3-1.root 4 >& ./fitParameters/LogFiles/gen_4.log
-#./fit angular_gen mc.gen ../RootFiles/MC_GENOnly/BToKMuMu_GENOnly_8TeV_genonly_v3-1.root 5 >& ./fitParameters/LogFile/sgen_5.log
-./fit angular_gen mc.gen ../RootFiles/MC_GENOnly/BToKMuMu_GENOnly_8TeV_genonly_v3-1.root 6 >& ./fitParameters/LogFiles/gen_6.log
-./fit angular_gen mc.gen ../RootFiles/MC_GENOnly/BToKMuMu_GENOnly_8TeV_genonly_v3-1.root 7 >& ./fitParameters/LogFiles/gen_7.log
-./fit angular_gen mc.gen ../RootFiles/MC_GENOnly/BToKMuMu_GENOnly_8TeV_genonly_v3-1.root 8 >& ./fitParameters/LogFiles/gen_8.log
-./fit angular_gen mc.gen ../RootFiles/MC_GENOnly/BToKMuMu_GENOnly_8TeV_genonly_v3-1.root 9 >& ./fitParameters/LogFiles/gen_9.log
-./fit angular_gen mc.gen ../RootFiles/MC_GENOnly/BToKMuMu_GENOnly_8TeV_genonly_v3-1.root 10 >& ./fitParameters/LogFiles/gen_10.log
+./fit angular_gen mc.gen ../RootFiles/MC_GENOnly/BToKMuMu_GENOnly_8TeV_genonly_v3-1.root 0 -b
+./fit angular_gen mc.gen ../RootFiles/MC_GENOnly/BToKMuMu_GENOnly_8TeV_genonly_v3-1.root 1 -b
+./fit angular_gen mc.gen ../RootFiles/MC_GENOnly/BToKMuMu_GENOnly_8TeV_genonly_v3-1.root 2 -b
+#./fit angular_gen mc.gen ../RootFiles/MC_GENOnly/BToKMuMu_GENOnly_8TeV_genonly_v3-1.root 3 -b
+./fit angular_gen mc.gen ../RootFiles/MC_GENOnly/BToKMuMu_GENOnly_8TeV_genonly_v3-1.root 4 -b
+#./fit angular_gen mc.gen ../RootFiles/MC_GENOnly/BToKMuMu_GENOnly_8TeV_genonly_v3-1.root 5 -b
+./fit angular_gen mc.gen ../RootFiles/MC_GENOnly/BToKMuMu_GENOnly_8TeV_genonly_v3-1.root 6 -b
+./fit angular_gen mc.gen ../RootFiles/MC_GENOnly/BToKMuMu_GENOnly_8TeV_genonly_v3-1.root 7 -b
+./fit angular_gen mc.gen ../RootFiles/MC_GENOnly/BToKMuMu_GENOnly_8TeV_genonly_v3-1.root 8 -b
+./fit angular_gen mc.gen ../RootFiles/MC_GENOnly/BToKMuMu_GENOnly_8TeV_genonly_v3-1.root 9 -b
+./fit angular_gen mc.gen ../RootFiles/MC_GENOnly/BToKMuMu_GENOnly_8TeV_genonly_v3-1.root 10 -b
 
-./fit angular_gen mc.gen ../RootFiles/MC_GENOnly/BToKMuMu_GENOnly_8TeV_genonly_v3-1.root 999 >& ./fitParameters/LogFiles/gen_999.log
+./fit angular_gen mc.gen ../RootFiles/MC_GENOnly/BToKMuMu_GENOnly_8TeV_genonly_v3-1.root 999 -b
 
 
 
