e7ec527e4af0cceb41460a11185c9f81615b1d1c
Geng Chen <normanaachen@gmail.com>
2015-01-25 09:25:14 +0100

    Collected command

---
+++ plugins/gen.sh
@@ -0,0 +1,17 @@
+
+./fit angular_gen mc.gen ../RootFiles/MC_GENOnly/BToKMuMu_GENOnly_8TeV_genonly_v3-1.root 0 >& ./fitParameters/LogFiles/gen_0.log
+./fit angular_gen mc.gen ../RootFiles/MC_GENOnly/BToKMuMu_GENOnly_8TeV_genonly_v3-1.root 1 >& ./fitParameters/LogFiles/gen_1.log
+./fit angular_gen mc.gen ../RootFiles/MC_GENOnly/BToKMuMu_GENOnly_8TeV_genonly_v3-1.root 2 >& ./fitParameters/LogFiles/gen_2.log
+#./fit angular_gen mc.gen ../RootFiles/MC_GENOnly/BToKMuMu_GENOnly_8TeV_genonly_v3-1.root 3 >& ./fitParameters/LogFile/sgen_3.log
+./fit angular_gen mc.gen ../RootFiles/MC_GENOnly/BToKMuMu_GENOnly_8TeV_genonly_v3-1.root 4 >& ./fitParameters/LogFiles/gen_4.log
+#./fit angular_gen mc.gen ../RootFiles/MC_GENOnly/BToKMuMu_GENOnly_8TeV_genonly_v3-1.root 5 >& ./fitParameters/LogFile/sgen_5.log
+./fit angular_gen mc.gen ../RootFiles/MC_GENOnly/BToKMuMu_GENOnly_8TeV_genonly_v3-1.root 6 >& ./fitParameters/LogFiles/gen_6.log
+./fit angular_gen mc.gen ../RootFiles/MC_GENOnly/BToKMuMu_GENOnly_8TeV_genonly_v3-1.root 7 >& ./fitParameters/LogFiles/gen_7.log
+./fit angular_gen mc.gen ../RootFiles/MC_GENOnly/BToKMuMu_GENOnly_8TeV_genonly_v3-1.root 8 >& ./fitParameters/LogFiles/gen_8.log
+./fit angular_gen mc.gen ../RootFiles/MC_GENOnly/BToKMuMu_GENOnly_8TeV_genonly_v3-1.root 9 >& ./fitParameters/LogFiles/gen_9.log
+./fit angular_gen mc.gen ../RootFiles/MC_GENOnly/BToKMuMu_GENOnly_8TeV_genonly_v3-1.root 10 >& ./fitParameters/LogFiles/gen_10.log
+
+./fit angular_gen mc.gen ../RootFiles/MC_GENOnly/BToKMuMu_GENOnly_8TeV_genonly_v3-1.root 999 >& ./fitParameters/LogFiles/gen_999.log
+
+
+
---
+++ plugins/reco.sh
@@ -0,0 +1,15 @@
+
+./fit angular_reco mc ../RootFiles/AllCut/MC_Signal_AllCuts_v3.root 0 >& ./fitParameters/LogFiles/reco_0.log
+./fit angular_reco mc ../RootFiles/AllCut/MC_Signal_AllCuts_v3.root 1 >& ./fitParameters/LogFiles/reco_1.log
+./fit angular_reco mc ../RootFiles/AllCut/MC_Signal_AllCuts_v3.root 2 >& ./fitParameters/LogFiles/reco_2.log
+#./fit angular_reco mc ../RootFiles/AllCut/MC_Signal_AllCuts_v3.root 3 >& ./fitParameters/LogFiles/reco_3.log
+./fit angular_reco mc ../RootFiles/AllCut/MC_Signal_AllCuts_v3.root 4 >& ./fitParameters/LogFiles/reco_4.log
+#./fit angular_reco mc ../RootFiles/AllCut/MC_Signal_AllCuts_v3.root 5 >& ./fitParameters/LogFiles/reco_5.log
+./fit angular_reco mc ../RootFiles/AllCut/MC_Signal_AllCuts_v3.root 6 >& ./fitParameters/LogFiles/reco_6.log
+./fit angular_reco mc ../RootFiles/AllCut/MC_Signal_AllCuts_v3.root 7 >& ./fitParameters/LogFiles/reco_7.log
+./fit angular_reco mc ../RootFiles/AllCut/MC_Signal_AllCuts_v3.root 8 >& ./fitParameters/LogFiles/reco_8.log
+./fit angular_reco mc ../RootFiles/AllCut/MC_Signal_AllCuts_v3.root 9 >& ./fitParameters/LogFiles/reco_9.log
+./fit angular_reco mc ../RootFiles/AllCut/MC_Signal_AllCuts_v3.root 10 >& ./fitParameters/LogFiles/reco_10.log
+
+./fit angular_reco mc ../RootFiles/AllCut/MC_Signal_AllCuts_v3.root 999 >& ./fitParameters/LogFiles/reco_999.log
+
